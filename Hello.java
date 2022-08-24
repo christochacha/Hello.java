de4b7a42af53c696c1bfe6404e2bf060ba0b8ea4
christochacha <112028584+christochacha@users.noreply.github.com>
2022-08-24 17:42:11 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/**********************
+* First Lab
+***********************
+* Christopher Chavez
+* 8/24
+* CMSC-255-004-FA2022
+***********************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Christopher Chavez>");
+		System.out.println("<First Lab>");
+		System.out.println("<Have all Java related applications and run a Java program>");
+		System.out.println("<August 24, 2022>");
+		System.out.println("<CMSC-255-004-FA2022>");
+		System.out.println();
+		
+	}
+}
